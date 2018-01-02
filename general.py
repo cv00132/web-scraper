f13bc5cb352b90a3f531c6b8aa9015d5633c1c15
Cory Vaughan <codercory13@gmail.com>
2018-01-02 10:41:21 -0500

    Wrote functions to append and delete data files. Also wrote a function to change files to set to save from duplicates and in case program closes prematurely

--- general.py
+++ general.py
@@ -6,10 +6,11 @@ def create_project_dir(directory):
         print('Creating project ' + directory)
         os.makedirs(directory)
 
+
 # Create queue and scraped files (if not created)
 def create_data_files(project_name, base_url):
-    queue = project_name + 'queue.txt'
-    scraped = project_name + 'scraped.txt'
+    queue = project_name + '/queue.txt'
+    scraped = project_name + '/scraped.txt'
     if not os.path.isfile(queue):
         write_file(queue, base_url)
     if not os.path.isfile(scraped):
@@ -20,3 +21,27 @@ def write_file(path, data):
     f = open(path, 'w')
     f.write(data)
     f.close()
+
+# Add data onto an existing files
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+# Delete the contents of a file
+def delete_file_contents(path):
+    with open(path, 'w'):
+        pass
+
+# Read a file and convert each line to set items
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
+    return results
+
+# Iterate through a set, each item will be a new line in the file
+def set_to_file(links, file):
+    delete_file_contents(file)
+    for link in sorted(links):
+        append_to_file(file, link)
