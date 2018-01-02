c820349b408839298a4c86a10dfcd5305434068e
Cory Vaughan <codercory13@gmail.com>
2018-01-02 10:21:45 -0500

    Wrote functions to create data files.

--- general.py
+++ general.py
@@ -6,4 +6,17 @@ def create_project_dir(directory):
         print('Creating project ' + directory)
         os.makedirs(directory)
 
-create_project_dir('testproject')
+# Create queue and scraped files (if not created)
+def create_data_files(project_name, base_url):
+    queue = project_name + 'queue.txt'
+    scraped = project_name + 'scraped.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(scraped):
+        write_file(scraped, '')
+
+# Writes a new files
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
